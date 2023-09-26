7d523daf65200769d59d6567545098bc7abf9671
niko.brusselaers <niko.brusselaers@student.ehb.be>
2023-09-26 14:14:35 +0200

    feat: created homepage
    created homepage of portfolio

---
+++ images/frontend/angular-portfolio-website/src/app/main/main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent {
+    introduction = "Greetings! I'm Niko Brusselaers"
+    content = "I'm a third-year student passionate about web development , striving to create immersive online experiences that blend creativity and functionality."
+}
