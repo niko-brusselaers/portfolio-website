608657ee879c12ca20cef611df28197f52d873c5
niko.brusselaers <niko.brusselaers@student.ehb.be>
2023-10-10 22:39:50 +0200

    refactor: updating style of website
    updating style to the new design of the website, updated main nav

--- images/frontend/angular-portfolio-website/src/app/app.module.ts
+++ images/frontend/angular-portfolio-website/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './core/shared/components/header/header.component';
+import { MainComponent } from './core/pages/home/main/main.component';
 
 @NgModule({
   declarations: [
