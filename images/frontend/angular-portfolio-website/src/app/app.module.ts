180afc6c3eb6ed759097e83cf70d4af92080ed07
niko.brusselaers <niko.brusselaers@student.ehb.be>
2023-10-11 10:29:11 +0200

    feat: created secondary nav
    created secondary nav for the new design and finished updating style of homepage content

--- images/frontend/angular-portfolio-website/src/app/app.module.ts
+++ images/frontend/angular-portfolio-website/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/shared/components/header/header.component';
 import { MainComponent } from './core/pages/home/main/main.component';
+import { SecondaryNavComponent } from './core/shared/components/secondary-nav/secondary-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    SecondaryNavComponent
   ],
   imports: [
     BrowserModule,
