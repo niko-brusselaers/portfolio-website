33e876ed5cb0632bb4926eacc4ac9b66fb4cd270
niko.brusselaers <niko.brusselaers@student.ehb.be>
2023-11-13 23:28:57 +0100

    style: created project page template

--- images/frontend/angular-portfolio-website/src/app/core/pages/project/project.component.ts
+++ images/frontend/angular-portfolio-website/src/app/core/pages/project/project.component.ts
@@ -9,12 +9,25 @@ import { ActivatedRoute } from '@angular/router';
 
 export class ProjectComponent {
 
-  projectName: string | undefined;
+    project: {
+      name: string, description: string,
+      technologies: string[], image: string,
+      link: string | undefined,
+      githubLink: string[]
+    } = {
+      name: "Portfolio",
+      description: "A portfolio website made with Angular and TailwindCSS. I wanted to create a website that would showcase my skills and projects. I also wanted to create a website that would be easy to maintain and update.  I used TailwindCSS to speed up the development process and to create a consistent design.",
+      technologies: ["Angular", "TailwindCSS","Typescript","NodeJs"],
+      image: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
+      link: "niko-brusselaers.com",
+      githubLink: ["github.com"]
+    }
 
-  constructor(private route: ActivatedRoute) {
-    this.projectName = route.snapshot.params['projectName']    
+  // projectName: string | undefined;
+
+  // constructor(private route: ActivatedRoute) {
+  //   this.projectName = route.snapshot.params['projectName']    
     
-}
 
   
 }
