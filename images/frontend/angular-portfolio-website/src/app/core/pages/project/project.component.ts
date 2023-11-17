609c6f314f8180bd675592be831f588c597339fe
niko.brusselaers <niko.brusselaers@outlook.com>
2023-11-17 23:51:17 +0100

    fix: fixed project data not loading in
    fixed issue where project data would not be displayed when viewing project page

--- images/frontend/angular-portfolio-website/src/app/core/pages/project/project.component.ts
+++ images/frontend/angular-portfolio-website/src/app/core/pages/project/project.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProject } from '../../shared/models/IProject';
 import { databaseService } from '../../services/databaseService';
@@ -10,31 +10,32 @@ import { databaseService } from '../../services/databaseService';
 })
 
 export class ProjectComponent {
+     project: IProject = {
+      name: "",
+      description: "",
+      technologies: [""],
+      image: "",
+      url: "",
+      githubLink: [""]
+    }
 
-    constructor(private route: ActivatedRoute) {
-  this.route.params.subscribe(params => {
-    const projectName = params['projectName'];
-    databaseService.getProjectDetails(projectName).then((project) => {
-      this.project = project;
-    });
+    constructor(private route: ActivatedRoute, private cdr: ChangeDetectorRef) {
+
+      
+
+      this.route.params.subscribe(params => {
+        const projectName = params['projectName'];
+         databaseService.getProjectDetails(projectName).then((project) => {
+          this.project = project;
+          console.log(project);
+          this.cdr.detectChanges();
+  });
   });
 }
 
 
-    project: IProject = {
-      name: "test",
-      description: "A portfolio website made with Angular and TailwindCSS. I wanted to create a website that would showcase my skills and projects. I also wanted to create a website that would be easy to maintain and update.  I used TailwindCSS to speed up the development process and to create a consistent design.",
-      technologies: ["Angular", "TailwindCSS","Typescript","NodeJs"],
-      image: "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png",
-      url: "niko-brusselaers.com",
-      githubLink: ["github.com"]
-    }
-
-  // projectName: string | undefined;
-
-  // constructor(private route: ActivatedRoute) {
-  //   this.projectName = route.snapshot.params['projectName']    
     
+
 
   
 }
--- images/frontend/angular-portfolio-website/src/app/core/services/databaseService.ts
+++ images/frontend/angular-portfolio-website/src/app/core/services/databaseService.ts
@@ -20,9 +20,9 @@ class DatabaseService{
 
     async getProjectDetails(projectName:string): Promise<IProject>{
         //get all projects 
-        const response = await this.getData();
+        const response = await this.getData();        
         //convert to json
-        const data: IProject[] = await response.json();
+        const data: IProject[] = await response.json();        
         //get project details
         const projectDetails: IProject = data.find((element: IProject) => element.name === projectName)!;
         //return project details
